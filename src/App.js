a19f653d1d137ee13c77f73a34cb2c5ef0acefdf
Jonas Wagner <jonas.zumpe@googlemail.com>
2024-11-15 17:03:07 +0100

    Setup strucutre and add baseline styles

--- src/App.js
+++ src/App.js
@@ -1,33 +1,40 @@
 import React from 'react';
+import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { CookiesProvider, useCookies } from 'react-cookie';
 import NineLamps from './games/nine_lamps/NineLamps';
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/nine-lamps">Nine Lamps</Link>
-            </li>
-          </ul>
-        </nav>
+    <CookiesProvider>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/nine-lamps">Nine Lamps</Link>
+              </li>
+            </ul>
+          </nav>
 
-        <Routes>
-          <Route path="/" element={
-            <div>
-              <h1>Welcome to RiddleBox</h1>
-              <p>Select a game from the menu.</p>
-            </div>
-          } />
-          <Route path="/nine-lamps" element={<NineLamps />} />
-        </Routes>
-      </div>
-    </Router>
+          <Routes>
+            <Route path="/" element={
+              <div className='homepage'>
+                <h1>Welcome to RiddleBox</h1>
+                <p>Select a game from the menu.</p>
+                <p>This page is still work in progress...</p>
+              </div>
+            } />
+            <Route path="/nine-lamps" element={
+                <NineLamps />
+              } />
+          </Routes>
+        </div>
+      </Router>
+    </CookiesProvider>
   );
 };
 
