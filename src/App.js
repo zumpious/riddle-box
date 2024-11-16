7fac4962a55f4b30e8fc479b252554c402029ada
Jonas Wagner <jonas.zumpe@googlemail.com>
2024-11-16 13:06:34 +0100

    Add simple burger menu

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import { CookiesProvider, useCookies } from 'react-cookie';
 import NineLamps from './games/nine_lamps/NineLamps';
 
 const App = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <CookiesProvider>
       <Router>
         <div>
-          <nav>
-            <ul>
+          <nav className="navbar">
+            <div className="navbar-brand">
+              <Link to="/">RiddleBox</Link>
+              <button className="burger-menu" onClick={toggleMenu}>
+                ☰
+              </button>
+            </div>
+            <ul className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
               <li>
-                <Link to="/">Home</Link>
+                <Link to="/" onClick={toggleMenu}>Home</Link>
               </li>
               <li>
-                <Link to="/nine-lamps">Nine Lamps</Link>
+                <Link to="/nine-lamps" onClick={toggleMenu}>Nine Lamps</Link>
               </li>
             </ul>
           </nav>
