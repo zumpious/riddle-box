8f3647e2027a22d7ffcb8b058caca27e4ffd18d4
Jonas Wagner <jonas.zumpe@googlemail.com>
2025-10-27 20:29:17 +0100

    Rename horse racing directory

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { CookiesProvider } from 'react-cookie'
 import NineLamps from './games/nine_lamps/NineLamps'
 import ColorCodeGuesser from './games/color_code_guesser/ColorCodeGuesser'
-import HorseRacing from './games/horse-racing/HorseRacing'
+import HorseRacing from './games/horse_racing/HorseRacing'
 import Menu from './components/menu/Menu'
 import Home from './screens/Home'
 
